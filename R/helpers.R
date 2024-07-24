04c6833ba494a848c83e02b8f7af65fdd36697aa
jbedia <joaquin.bedia@gmail.com>
2024-07-24 10:08:31 +0200

    'New' SSPs defined in ipcc_terms v0.7

--- R/helpers.R
+++ R/helpers.R
@@ -10,13 +10,13 @@ set.exp.nodename <- function(exp) {
                                       "ssp5_3_4os"))
     switch(exp,
            "historical" = "ipcc:Historical",
-           "ssp1_1_9" = "ipcc:SSP119", ## To be defined
+           "ssp1_1_9" = "ipcc:SSP119",
            "ssp1_2_6" = "ipcc:SSP126",
            "ssp2_4_5" = "ipcc:SSP245",
            "ssp5_8_5" = "ipcc:SSP585",
            "ssp3_7_0" = "ipcc:SSP370",
-           "ssp4_3_4" = "ipcc:SSP434", ## To be defined
+           "ssp4_3_4" = "ipcc:SSP434",
            "ssp4_6_0" = "ipcc:SSP460",
-           "ssp5_3_4os" = "ipcc:SSP534os" ## To be defined
+           "ssp5_3_4os" = "ipcc:SSP534-os"
     )
 }
