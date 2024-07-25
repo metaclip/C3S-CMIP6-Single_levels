a9c6aecb31578ba17c5f5f751a6a1cd046d87233
jbedia <joaquin.bedia@gmail.com>
2024-07-25 18:48:44 +0200

    Minor annotation

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,5 @@
 library(magrittr)
-library(metaclipR)  ## >= v1.4.0 ## remotes::install_github("METACLIP/metaclipR")
+library(metaclipR)  ## >= v1.5.1 ## remotes::install_github("METACLIP/metaclipR")
 library(igraph)
 
 ## LOAD MASTER TABLES
